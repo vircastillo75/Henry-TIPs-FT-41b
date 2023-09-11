d1127ff5ff77e9c17af1b069ba8a49eb9eda2f31
María Viriginia castillo <127612492+vircastillo75@users.noreply.github.com>
2023-09-11 17:01:25 -0300

    check

--- Ejercicios/01-FindNeedle/FindNeedle.js
+++ Ejercicios/01-FindNeedle/FindNeedle.js
@@ -1,5 +1,22 @@
 function FindNeedle(haystack, needle) {
   // Your code here:
+ /*OPCION 1
+  for (let i = 0; i <haystack.length; i++){
+    if (haystack.slice(i, i + needle.length) === needle) return i;
+  }
+  return -1;*/
+
+  //OPCION  2
+  for (let i = 0; i < haystack.length; i++) {
+    if( haystack[i] === needle[0]){
+      let aux = haystack.slice(i, i + needle.length);
+      if ( aux === needle) return i;
+    }
+  }
+  return -1;
+  //OPCION TRAMPA
+  //return haystack.search(needle)
 }
+
 
 module.exports = FindNeedle;
